f5869cf6f6369c9aea498ee8019d0d06c75e2fbd
ThomasRaymond <46885409+ThomasRaymond@users.noreply.github.com>
2023-01-16 11:23:34 +0100

    debut depart

--- src/main/java/model/Depart.java
+++ src/main/java/model/Depart.java
@@ -1,4 +1,21 @@
 package model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
 public class Depart {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int IDDepart;
+
+    @Temporal(TemporalType.DATE)
+    private String dateDepart;
+
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "depart")
+    private List<Personnel> listePersonnel;
+
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "depart")
+    private List<Passager> listePassager;
+
 }
